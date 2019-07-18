4086cfefb88772535e23ab8df1e73dee9fde34e0
BernabeKumul <52230020+BernabeKumul@users.noreply.github.com>
2019-07-17 20:46:45 -0500

    Se creo el codigo para mostrar los platillos

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,24 +7,6 @@ function Header(){
         <header className="stick">
                 <div className="topbar">
                     <div className="container">
-                        {/* <div className="select-wrp">
-                            <select data-placeholder="Feel Like Eating">
-                                <option>FEEL LIKE EATING</option>
-                                <option>Burger</option>
-                                <option>Pizza</option>
-                                <option>Fried Rice</option>
-                                <option>Chicken Shots</option>
-                            </select>
-                        </div>
-                        <div className="select-wrp">
-                            <select data-placeholder="Choose Location">
-                                <option>CHOOSE LOCATION</option>
-                                <option>New york</option>
-                                <option>Washington</option>
-                                <option>Chicago</option>
-                                <option>Los Angeles</option>
-                            </select>
-                        </div> */}
                         <div className="topbar-register">
                             <a className="log-popup-btn" href="#" title="Login" >LOGIN</a> / <a className="sign-popup-btn" href="#" title="Register" >REGISTER</a>
                         </div>
--- src/components/Platillo.js
+++ src/components/Platillo.js
@@ -1,30 +1,28 @@
 import React from 'react';
 import imagen from '../images/resource/popular-dish-img1.jpg';
 
-function Platillo() {
+function Platillo({_id,oComida}) {
+    console.log(oComida.cover_photo)
     return (
         <div className="col-md-4 col-sm-6 col-lg-4 mt-4">
             <div className="popular-dish-box wow fadeIn" data-wow-delay="0.2s">
                 <div className="popular-dish-thumb">
                     <a href="food-detail.html" title="" >
-                    <img src={imagen} alt="preview" className="d-block"/>
-                        {/* <img src="assets/images" alt="popular-dish-img1.jpg" itemprop="image"> */}
+                    <img src={oComida.cover_photo} alt="preview" className="d-block"/>
                     </a>
                     <span className="post-rate yellow-bg brd-rd2"><i className="fa fa-star-o"></i> 4.25</span>
                     <span className="post-likes brd-rd4"><i className="fa fa-heart-o"></i> 12</span>
                 </div>
                 <div className="popular-dish-info">
-                    <h4 ><a href="food-detail.html" title="" >
-                        Maenaam Thai Restaurant</a>
+                    <h4 >
+                       {oComida.nombre}
                     </h4>
-                    <p >Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
-                    <span className="price">$85.00</span>
+                    <p >{oComida.descripcion}</p>
+                    <span className="price">$ {oComida.precio}</span>
                     <a className="brd-rd2" href="food-detail.html" title="Order Now" >Order Now</a>
                     <div className="restaurant-info">
-                        {/* <img src="assets/images/resource/restaurant-logo1.png" alt="restaurant-logo1.png" itemprop="image"> */}
                         <div className="restaurant-info-inner">
-                            <h6 ><a href="restaurant-detail.html" title="" >Fabio al Porto Ristorante</a></h6>
-                            <span className="red-clr">5th Avenue New York 68</span>
+                            <span className="red-clr">Restaurante: Pla</span>
                         </div>
                     </div>
                 </div>
